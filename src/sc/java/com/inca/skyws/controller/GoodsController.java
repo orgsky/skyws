7cb34af9641e895c1d4d3bb6f1d8f86070dc8ec9
史战伟 <shizhanwei123@126.com>
2018-09-07 07:53:23 +0800

    添加商品列表代码
    Signed-off-by: 史战伟 <shizhanwei123@126.com>

--- src/sc/java/com/inca/skyws/controller/GoodsController.java
+++ src/sc/java/com/inca/skyws/controller/GoodsController.java
@@ -1,5 +1,11 @@
 package com.inca.skyws.controller;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -8,18 +14,41 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.inca.skyws.model.GoodsInfo;
+import com.inca.skyws.model.UserInfo;
+import com.inca.skyws.system.LoginUser;
+
 @Controller
 @RequestMapping("/goods")
 public class GoodsController {
 	private static final Logger log = LoggerFactory.getLogger(GoodsController.class);
 
-
-	@RequestMapping("/")
+	@RequestMapping("/mygoods")
 	public String home(Model model, HttpServletRequest req) {
-		log.info("访问商城主页....");
-		return "goods";
-	}
 
-	
+		List<GoodsInfo> goodsList = new ArrayList<GoodsInfo>();
+		LoginUser loginUser = LoginUser.getLoginUser();
+		for (int row = 0; row < 8; row++) {
+			GoodsInfo goods = new GoodsInfo();
+			goods.setId(row);
+			goods.setSpec("1台/箱");
+			goods.setUnit("台");
+			goods.setClickNum("20" + row);
+			goods.setMajPic("/static/img/tv.jpg");
+			goods.setPic("/static/img/tv.jpg");
+			goods.setGoodsCode("60SU470A");
+			goods.setGoodsName("夏普 (SHARP)LCD-70SU575A");
+			goods.setWeight(new BigDecimal("80"));
+			goods.setPrice(new BigDecimal("3999.00"));
+			goods.setStock(99+row);
+			UserInfo user=new UserInfo();
+			user.setId(loginUser.getId());
+			user.setUsercode(loginUser.getUsercode());
+			user.setUsername(loginUser.getUsername());
+			goodsList.add(goods);
+		}
+		model.addAttribute("goodsList", goodsList);
+		return "mygoodslist";
+	}
 
 }
--- src/sc/java/com/inca/skyws/model/GoodsInfo.java
+++ src/sc/java/com/inca/skyws/model/GoodsInfo.java
@@ -1,78 +1,30 @@
-package com.inca.skyws.bean;
+package com.inca.skyws.model;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
+import com.inca.skyws.bean.User;
 import com.inca.skyws.bean.annotation.Title;
 
-@Title("商品表")
-@Entity(name = "pub_goods")
-public class Goods extends BaseEntity {
-	private static final long serialVersionUID = 7422039013630421256L;
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+public class GoodsInfo {
 	private Integer id;
-
-	@Title("商品编码")
-	@Column(nullable = false, unique = true)
 	private String goodsCode;
-
-	@Title("商品名")
-	@Column(nullable = false)
 	private String goodsName;
-
-	@Title("单位")
-	@Column(nullable = false)
 	private String unit;
-
-	@Title("规格")
-	@Column(nullable = false)
 	private String spec;
-
-	@Title("价格")
-	@Column(precision = 20, scale = 10)
 	private BigDecimal price;
-
-	@Title("重量")
-	@Column(precision = 20, scale = 10)
 	private BigDecimal weight;
-
-	@Title("库存")
-	private Integer stock;
-
-	@Title("图片")
 	private String pic;
-
-	@Title("宣传图片")
 	private String majPic;
-
-	@Title("商品介绍")
-	@Lob
 	private String goodsProfile;
-
-	@Title("点击次数")
 	private String clickNum;
-
-	@Title("商品评论")
-	@OneToMany(mappedBy = "goods", fetch = FetchType.LAZY)
-	private List<GoodsReview> reviews;
-
-	@Title("卖家")
-	@OneToOne
-	@JoinColumn(name = "seller_id", referencedColumnName = "id", nullable = false)
-	private User seller;
+	private UserInfo seller;
+	private Integer stock;
 
 	public Integer getId() {
 		return id;
@@ -162,19 +114,11 @@ public class Goods extends BaseEntity {
 		this.clickNum = clickNum;
 	}
 
-	public List<GoodsReview> getReviews() {
-		return reviews;
-	}
-
-	public void setReviews(List<GoodsReview> reviews) {
-		this.reviews = reviews;
-	}
-
-	public User getSeller() {
+	public UserInfo getSeller() {
 		return seller;
 	}
 
-	public void setSeller(User seller) {
+	public void setSeller(UserInfo seller) {
 		this.seller = seller;
 	}
 
