e5ca73aa86a6c8513afe56964107a52a835cf75e
shizhanwei <shizhanwei@inca.com.cn>
2018-09-06 11:47:29 +0800

    增加富文本编辑器资源请求类 Signed-off-by: shizhanwei <shizhanwei@inca.com.cn>

--- src/ueditor/java/com/inca/editor/controller/UEditorController.java
+++ src/ueditor/java/com/inca/editor/controller/UEditorController.java
@@ -1,48 +1,43 @@
 package com.inca.editor.controller;
- 
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.inca.editor.ActionEnter;
- 
+
 /**
  * Created by ldb on 2017/4/9.
  */
 @Controller
 public class UEditorController {
- 
- 
-    @RequestMapping("/")
-    private String showPage(){
-        return "index";
-    }
-    
-    @RequestMapping(value="/static")
-    public void config(HttpServletRequest request, HttpServletResponse response) {
-        response.setContentType("application/json");
-        try {
-            String exec = new ActionEnter(request).exec();
-            PrintWriter writer = response.getWriter();
-            writer.write(exec);
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    
-    @RequestMapping("/save")
-	private String save(String content) {
-		System.out.println("content=" + content);
+	private static final Logger log = LoggerFactory.getLogger(UEditorController.class);
+
+	@RequestMapping("/")
+	private String showPage() {
 		return "index";
 	}
 
- 
+	@RequestMapping("/ueditor")
+	public void config(HttpServletRequest request, HttpServletResponse response) {
+		response.setContentType("application/json");
+		try {
+			log.info("百度富文本编辑器请求资源：" + request.getParameter("action"));
+			String exec = new ActionEnter(request).exec();
+			PrintWriter writer = response.getWriter();
+			writer.write(exec);
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			log.info("百度富文本编辑器请求资源，异常：" + e.getMessage(), e);
+		}
+	}
+
 }
