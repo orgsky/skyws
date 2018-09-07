b322687dccf6f91c504734bdb7c878a9d8f77611
史战伟 <shizhanwei123@126.com>
2018-09-08 03:46:13 +0800

    商品管理静态代码 Signed-off-by: 史战伟 <shizhanwei123@126.com>

--- src/sc/java/com/inca/skyws/controller/GoodsController.java
+++ src/sc/java/com/inca/skyws/controller/GoodsController.java
@@ -25,7 +25,10 @@ public class GoodsController {
 
 	@RequestMapping("/mygoods")
 	public String home(Model model, HttpServletRequest req) {
-
+		String keycode = req.getParameter("keycode");
+		log.info("keycode=" + keycode);
+		String keyname = req.getParameter("keyname");
+		log.info("keyname=" + keyname);
 		List<GoodsInfo> goodsList = new ArrayList<GoodsInfo>();
 		LoginUser loginUser = LoginUser.getLoginUser();
 		for (int row = 0; row < 8; row++) {
@@ -40,8 +43,8 @@ public class GoodsController {
 			goods.setGoodsName("夏普 (SHARP)LCD-70SU575A");
 			goods.setWeight(new BigDecimal("80"));
 			goods.setPrice(new BigDecimal("3999.00"));
-			goods.setStock(99+row);
-			UserInfo user=new UserInfo();
+			goods.setStock(99 + row);
+			UserInfo user = new UserInfo();
 			user.setId(loginUser.getId());
 			user.setUsercode(loginUser.getUsercode());
 			user.setUsername(loginUser.getUsername());
@@ -49,6 +52,31 @@ public class GoodsController {
 		}
 		model.addAttribute("goodsList", goodsList);
 		return "mygoodslist";
+	}
+
+	@RequestMapping("/goodsinfo")
+	public String goodsInfo(Model model, HttpServletRequest req) {
+		String id = req.getParameter("id");
+		log.info("id=" + id);
+		LoginUser loginUser = LoginUser.getLoginUser();
+		GoodsInfo goods = new GoodsInfo();
+		goods.setId(Integer.valueOf(id));
+		goods.setSpec("1台/箱");
+		goods.setUnit("台");
+		goods.setClickNum("20" + Integer.valueOf(id));
+		goods.setMajPic("/static/img/tv.jpg");
+		goods.setPic("/static/img/tv.jpg");
+		goods.setGoodsCode("60SU470A");
+		goods.setGoodsName("夏普 (SHARP)LCD-70SU575A");
+		goods.setWeight(new BigDecimal("80"));
+		goods.setPrice(new BigDecimal("3999.00"));
+		goods.setStock(99 + Integer.valueOf(id));
+		UserInfo user = new UserInfo();
+		user.setId(loginUser.getId());
+		user.setUsercode(loginUser.getUsercode());
+		user.setUsername(loginUser.getUsername());
+		model.addAttribute("goods", goods);
+		return "goodsinfo";
 	}
 
 }
