610fceca70eb9fda3861c73e9f47bedcc435b06e
shizhanwei <shizhanwei@inca.com.cn>
2018-09-27 11:00:36 +0800

    删除部分文件 Signed-off-by: shizhanwei <shizhanwei@inca.com.cn>

--- src/main/java/com/inca/skyws/SkyApplication.java
+++ src/main/java/com/inca/skyws/SkyApplication.java
@@ -1,10 +1,33 @@
 package com.inca.skyws;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 public class SkyApplication {
+
+	@Bean
+	public Object testBean(PlatformTransactionManager platformTransactionManager) {
+		System.out.println(">>>>>>>>>>" + platformTransactionManager.getClass().getName());
+		return new Object();
+	}
+
+	@Bean
+	public PlatformTransactionManager txManager(DataSource dataSource) throws SQLException {
+		System.out.println(">>>>>>>>>>"+dataSource);
+		
+		System.out.println(">>>>>>>>>>"+dataSource.getConnection().getClientInfo());
+		return new DataSourceTransactionManager(dataSource);
+	}
 
 	public static void main(String[] args) {
 		System.err.println("项目启动...");
